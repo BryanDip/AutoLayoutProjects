a51c9aacabb95f17b3c7d738ced3a3c789d49dfa
Bryan Dipasupil <Bry@bryans-air>
2016-11-03 15:22:48 -0700

    Added VFL

--- Auto Layout Projects/VisualFormatLanguageVC.swift
+++ Auto Layout Projects/VisualFormatLanguageVC.swift
@@ -14,18 +14,44 @@ class VisualFormatLanguageVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+    // Visual Format Language (VFL)
+        
         let myView = UIView()
         myView.backgroundColor = UIColor.red
         myView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(myView)
         
-        let views = ["myView" : myView]
+        let views = ["myView" : myView, "super" : view]
         
-        let constraints1 = NSLayoutConstraint.constraints(withVisualFormat: "[myView(100,<=50)]|", options: .alignAllTop, metrics: nil, views: views)
+        let constraints1 = NSLayoutConstraint.constraints(withVisualFormat: "|[myView]|", options: .alignAllTop, metrics: nil, views: views)
         let constraints2 = NSLayoutConstraint.constraints(withVisualFormat: "V:[myView(50)]|", options: .alignAllTop, metrics: nil, views: views)
         
         NSLayoutConstraint.activate(constraints1)
         NSLayoutConstraint.activate(constraints2)
+        
+        
+    // VFL Spacing between views
+        
+        let button1 = UIButton()
+        button1.backgroundColor = UIColor.green
+        button1.setTitle("buton1", for: .normal)
+        button1.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button1)
+        
+        let button2 = UIButton()
+        button2.backgroundColor = UIColor.yellow
+        button2.setTitle("button2", for: .normal)
+        button2.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button2)
+        
+        let views1 = ["button1" : button1, "button2" : button2]
+        
+        let constraints3 = NSLayoutConstraint.constraints(withVisualFormat: "|-50-[button1]-30-[button2]", options: .alignAllTop, metrics: nil, views: views1)
+        let constraints4 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-100-[button1]", options: .alignAllTop, metrics: nil, views: views1)
+        NSLayoutConstraint.activate(constraints3)
+        NSLayoutConstraint.activate(constraints4)
+        
+        button2.heightAnchor.constraint(equalToConstant: 100).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
